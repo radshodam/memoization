8334938cd6469f828f10e32c34e6be500a496a23
mohamadZareai 	☾ <mohamadzareai@gmail.com>
2023-04-04 22:45:58 +0430

    add example 2 useref state

--- memoization/pages/ex8/index.jsx
+++ memoization/pages/ex8/index.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function Index() {
-  const userNameRef = useRef();
-  const titleRef = useRef();
-  useEffect(() => {
-    console.log("userNameRef", userNameRef);
-    userNameRef.current.focus();
-  }, []);
+  //state
+  const [course, setCourse] = useState("JS");
+  const courseRef = useRef("Npm");
+
+  const changeState = () => {
+    setCourse("React Js");
+  };
+  const changeRef = () => {
+    courseRef.current = "Vue Js";
+    console.log(courseRef);
+  };
 
   return (
     <div>
-      <h3 ref={titleRef}>lorenzo</h3>
-      <input ref={userNameRef} type="text" />
+      <h3>Course(State): {course}</h3>
+      <hr />
+      <h3>Course(Ref): {courseRef.current}</h3>
+      <hr />
+      <button onClick={changeState}>Update Course (State)</button>
+      <button onClick={changeRef}>Update Course (Ref)</button>
     </div>
   );
 }
